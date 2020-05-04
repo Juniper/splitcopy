b32a227936a1f194347ad1df08e946ea16e0003a
Chris Jenn <crj@juniper.net>
2020-05-04 14:13:25 +1000

    fix setup.py console_scripts to point to correct location

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="splitcopy",
-    version='1.0.15.1',
+    version='1.0.15.2',
     url="https://github.com/Juniper/splitcopy",
     author="Chris Jenn",
     author_email="jnpr-community-netdev@juniper.net",
@@ -17,7 +17,7 @@ setup(
     install_requires=['junos-eznc>=2.3.0'],
     entry_points={
         'console_scripts': [
-            'splitcopy=splitcopy:main',
+            'splitcopy=splitcopy.splitcopy:main',
         ],
     },
     classifiers=[
