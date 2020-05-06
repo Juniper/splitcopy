5d0ceb9709a2731e86f0756faf66c68f6cac9305
Chris Jenn <crj@juniper.net>
2020-05-07 03:01:30 +1000

    refactoring paramikoshell.py

--- splitcopy/paramikoshell.py
+++ splitcopy/paramikoshell.py
@@ -67,39 +67,45 @@ class SSHShell:
             :return: paramiko.SSHClient instance
         """
         logger.debug("entering session_open()")
-        config = {}
-        kwargs = {}
+        kwargs = {"hostname": self.host, "username": self.user}
         ssh_client = paramiko.SSHClient()
         ssh_client.load_system_host_keys()
         ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
         ssh_config = os.path.expanduser("~/.ssh/config")
+        ask_pass = False
+        key_found = False
+        if (
+            os.path.isfile(os.path.expanduser("~/.ssh/id_rsa"))
+            or os.path.isfile(os.path.expanduser("~/.ssh/id_dsa"))
+            or os.path.isfile(os.path.expanduser("~/.ssh/id_ecdsa"))
+        ):
+            key_found = True
 
-        if ssh_config:
+        if os.path.isfile(ssh_config):
             config = paramiko.SSHConfig()
             with open(ssh_config) as open_ssh_config:
                 config.parse(open_ssh_config)
             config = config.lookup(self.host)
-
-        if config.get("proxycommand"):
-            self._sock =  paramiko.proxy.ProxyCommand(config.get("proxycommand"))
+            if config.get("proxycommand"):
+                self._sock = paramiko.proxy.ProxyCommand(config.get("proxycommand"))
+                kwargs.update({"sock": self._sock})
 
         agent = paramiko.Agent()
         agent_keys = agent.get_keys()
         logger.debug("ssh agent has {} keys".format(len(agent_keys)))
-        ask = True
 
         if self.passwd is not None:
             kwargs.update(
                 {"password": self.passwd, "allow_agent": False, "look_for_keys": False}
             )
-            ask = False
         elif self.user != getpass.getuser():
             print(
-                "skipping key based auth as {} != {}".format(
+                "skipping publickey ssh auth as {} != {}".format(
                     self.user, getpass.getuser()
                 )
             )
             kwargs.update({"allow_agent": False, "look_for_keys": False})
+            ask_pass = True
         elif self.key_filename is not None:
             kwargs.update(
                 {
@@ -107,30 +113,37 @@ class SSHShell:
                     "allow_agent": False,
                     "look_for_keys": False,
                     "password": None,
-                    "passphrase": getpass.getpass(
-                        prompt="ssh key passphrase (Enter for None): ", stream=None
-                    ),
                 }
             )
-            ask = False
-        elif len(agent_keys) == 0:
+            # paramiko is a little broken (see github issue #1664) 
+            # work around by always asking for passphrase here
+            # else "SSHException: encountered RSA key, expected OPENSSH key" error
+            # when key has passphrase
+            passphrase = getpass.getpass(
+                prompt="ssh key passphrase (Enter for None): ", stream=None
+            )
+            if passphrase != "":
+                kwargs.update({"passphrase": passphrase})
+        elif len(agent_keys) == 0 and not key_found:
+            print("no ssh keys found, nor ssh agent running, skipping publickey ssh auth")
             kwargs.update({"allow_agent": False, "look_for_keys": False})
-        elif len(agent_keys) > 0:
-            ask = False
+            ask_pass = True
 
-        if ask:
+        if ask_pass:
             self.passwd = getpass.getpass(
                 prompt="{}@{}'s password: ".format(self.user, self.host), stream=None
             )
             kwargs["password"] = self.passwd
 
-        kwargs.update(
-            {"sock": self._sock, "hostname": self.host, "username": self.user}
-        )
-        ssh_client.connect(**kwargs)
-
-        # ensure session stays up
-        ssh_client._transport.set_keepalive(60)
+        try:
+            ssh_client.connect(**kwargs)
+        except PasswordRequiredException:
+            passphrase = getpass.getpass(
+                prompt="ssh key passphrase (Enter for None): ", stream=None
+            )
+            if passphrase != "":
+                kwargs.update({"passphrase": passphrase})
+                ssh_client.connect(**kwargs)
         return ssh_client
 
     def get_transport(self, ssh_client):
@@ -177,14 +190,17 @@ class SSHShell:
             :param cmd: cmd to be sent over the channel
             :type: string
         """
-        self._chan.send(cmd + "\n")
+        self._chan.send("{}\n".format(cmd))
         logger.debug("sent '{}'".format(cmd))
 
     def close(self):
-        """ terminates the session
+        """ terminates both the channel (if present) and the underlying session
             :return: None
         """
-        self._session.close()
+        if self._chan is not None:
+            self._chan.close()
+        if self._session is not None:
+            self._session.close()
 
     def run(self, cmd, timeout=30, exitcode=True):
         """ sends a cmd to remote host, if exitcode is True will check its exit status
