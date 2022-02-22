c4c7f99d6b35d9cf4e48eea08c04419e52be9636
Chris Jenn <crj@juniper.net>
2022-02-22 23:54:24 +1100

    add support for resuming transfers

--- splitcopy/ftp.py
+++ splitcopy/ftp.py
@@ -16,26 +16,32 @@ logger = logging.getLogger(__name__)
 
 
 class FTP(ftplib.FTP):
-    """ FTP utility used to transfer files to and from hosts
-        mostly ripped from py-junos-eznc (with permission)
+    """FTP utility used to transfer files to and from hosts
+    mostly ripped from py-junos-eznc (with permission)
     """
 
-    def __init__(self, **kwargs):
-        """ initialize the FTP class
-            :param kwargs: named arguments
-            :type dict:
+    def __init__(self, file_size=None, progress=None, **kwargs):
+        """initialize the FTP class
+        :param file_size: size of file to transfer
+        :type int:
+        :param progress: Progress (from progress.py)
+        :type object:
+        :param kwargs: named arguments
+        :type dict:
         """
         host = kwargs.get("host")
         user = kwargs.get("user")
         passwd = kwargs.get("passwd")
-        self.callback = kwargs.get("progress")
         timeout = kwargs.get("timeout")
         if not timeout:
             timeout = 30
+        if logger.getEffectiveLevel() == 10:
+            self.set_debuglevel(level=1)
+        ftplib.FTP.__init__(self, host=host, user=user, passwd=passwd, timeout=timeout)
+        self.file_size = file_size
+        self.progress = progress
         self.header_bytes = 33
         self.sent = 0
-        self.file_size = 0
-        ftplib.FTP.__init__(self, host=host, user=user, passwd=passwd, timeout=timeout)
 
     def __enter__(self):
         return self
@@ -43,47 +49,53 @@ class FTP(ftplib.FTP):
     def __exit__(self, exc_ty, exc_val, exc_tb):
         self.quit()
 
-    def put(self, local_file, remote_file):
-        """ copies file from local host to remote host
-            :param local_file: path to local file
-            :type: string
-            :param remote_file: full path on server
-            :type: string
+    def put(self, local_file, remote_file, restart_marker):
+        """copies file from local host to remote host
+        :param local_file: path to local file
+        :type: string
+        :param remote_file: full path on server
+        :type: string
         """
         with open(local_file, "rb") as open_local_file:
+            if restart_marker is not None:
+                self.sent = restart_marker
+                open_local_file.seek(restart_marker,0)
 
             def callback(data):
-                if self.callback:
-                    size_data = sys.getsizeof(data) - self.header_bytes
-                    self.sent += size_data
-                    self.callback(
-                        file_name=os.path.basename(local_file),
-                        file_size=self.file_size,
-                        sent=self.sent,
-                    )
+                size_data = sys.getsizeof(data) - self.header_bytes
+                self.sent += size_data
+                self.progress.report_progress(
+                    file_name=os.path.basename(local_file),
+                    file_size=self.file_size,
+                    sent=self.sent,
+                )
 
             self.storbinary(
-                cmd="STOR " + remote_file, fp=open_local_file, callback=callback
+                cmd="STOR " + remote_file,
+                fp=open_local_file,
+                callback=callback,
+                rest=restart_marker,
             )
 
-    def get(self, remote_file, local_file):
-        """ copies file from remote host to local host
-            :param remote_file: full path on server
-            :type: string
-            :param local_file:  path to local file
-            :type: string
+    def get(self, remote_file, local_file, restart_marker=None):
+        """copies file from remote host to local host
+        :param remote_file: full path on server
+        :type: string
+        :param local_file:  path to local file
+        :type: string
         """
-        with open(local_file, "wb") as local_fh:
-
+        if restart_marker is not None:
+            self.sent = restart_marker
+        with open(local_file, "ab") as open_local_file:
             def callback(data):
-                local_fh.write(data)
-                if self.callback:
-                    size_data = sys.getsizeof(data) - self.header_bytes
-                    self.sent += size_data
-                    self.callback(
-                        file_name=os.path.basename(local_file),
-                        file_size=self.file_size,
-                        sent=self.sent,
-                    )
+                open_local_file.write(data)
+                size_data = sys.getsizeof(data) - self.header_bytes
+                self.sent += size_data
+                self.progress.report_progress(
+                    file_name=os.path.basename(local_file),
+                    file_size=self.file_size,
+                    sent=self.sent,
+                )
 
-            self.retrbinary("RETR " + remote_file, callback)
+            self.retrbinary("RETR " + remote_file, callback, rest=restart_marker)
+
